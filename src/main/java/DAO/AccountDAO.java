0a70d0c0a05b97272dd5f586f4a0f081a69d5fdd
Kevin Moore <kevinmoore444@gmail.com>
2023-07-12 23:57:45 +0000

    Worked on DAO

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,64 @@
+package DAO;
+import Util.ConnectionUtil;
+import Model.Account;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+// User Registration
+// As a user, I should be able to create a new Account on the endpoint POST localhost:8080/register. 
+// The body will contain a representation of a JSON Account, but will not contain an account_id.
+// The registration will be successful if and only if the username is not blank, the password is at 
+// least 4 characters long, and an Account with that username does not already exist. 
+// If all these conditions are met, the response body should contain a JSON of the Account, including its account_id. 
+// The response status should be 200 OK, which is the default. The new account should be persisted to the database.
+// If the registration is not successful, the response status should be 400. (Client error)
+
+// Login
+// As a user, I should be able to verify my login on the endpoint POST localhost:8080/login. 
+// The request body will contain a JSON representation of an Account, not containing an account_id. 
+// In the future, this action may generate a Session token to allow the user to securely use the site. 
+// We will not worry about this for now.
+// The login will be successful if and only if the username and password provided in the request body JSON 
+// match a real account existing on the database. 
+// If successful, the response body should contain a JSON of the account in the response body, including its account_id. 
+// The response status should be 200 OK, which is the default.
+// If the login is not successful, the response status should be 401. (Unauthorized)
+
+
+
+
+public class AccountDAO {
+    
+    //Create New Account
+    public Account createAccount(Account newAccount){
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            //Create prepared statement. 
+            String sql = "INSERT INTO account (username, password) VALUES (?, ?)" ;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, newAccount.getUsername());
+            preparedStatement.setString(2, newAccount.getPassword());
+
+            //Execute update
+            preparedStatement.executeUpdate();
+            
+            //Retrieve and unpack the response. If there is a response, use it to create a new account object with the generated ID  
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if(pkeyResultSet.next()){
+                int generated_message_id = (int) pkeyResultSet.getLong(1);
+                return new Account(generated_message_id, newAccount.getUsername(), newAccount.getPassword());
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+
+
+
+}
---
+++ src/main/java/DAO/MessageDAO.java
@@ -0,0 +1,200 @@
+package DAO;
+import Util.ConnectionUtil;
+import Model.Message;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class MessageDAO {
+    
+    //Create New Message
+    public Message createMessage(Message message){
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            //Create prepared statement. 
+            String sql = "INSERT INTO message (posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?)" ;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, message.getPosted_by());
+            preparedStatement.setString(2, message.getMessage_text());
+            preparedStatement.setLong(3, message.getTime_posted_epoch());
+
+            //Execute update
+            preparedStatement.executeUpdate();
+            
+            //Retrieve and unpack the response. If there is a response, use it to create a new message object with the generated ID and 
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if(pkeyResultSet.next()){
+                int generated_message_id = (int) pkeyResultSet.getLong(1);
+                return new Message(generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    //Get All messages
+    public List<Message> getAllMessages(){
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        try {
+
+            //Create a prepared Statement
+            String sql = "SELECT * FROM message";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            //Execute Statement
+            ResultSet rs = preparedStatement.executeQuery();
+
+            //With every row in the response, create a new message object and append it to our list of messages. 
+            while(rs.next()){
+                Message message = new Message(rs.getInt("message_id"),
+                        rs.getInt("posted_by"),
+                        rs.getString("message_text"),
+                        rs.getLong("time_posted_epoch"));
+                messages.add(message);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return messages;
+    }
+
+    // Get All Messages From User Given Account Id
+    public List<Message> getAllUserMessages(int posted_by){
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        try {
+
+            //Create a prepared Statement
+            String sql = "SELECT * FROM message WHERE posted_by = (?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, posted_by);
+
+            //Execute Statement
+            ResultSet rs = preparedStatement.executeQuery();
+
+            //With every row in the response, create a new message object and append it to our list of messages. 
+            while(rs.next()){
+                Message message = new Message(rs.getInt("message_id"),
+                        rs.getInt("posted_by"),
+                        rs.getString("message_text"),
+                        rs.getLong("time_posted_epoch"));
+                messages.add(message);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return messages;
+    }
+
+    //Get One Message
+    public Message getMessageById(int message_id){
+        Connection connection = ConnectionUtil.getConnection();
+        
+        try {
+            //Create a prepared Statement
+            String sql = "SELECT * FROM message WHERE message_id = (?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, message_id);
+
+            //Execute Statement
+            ResultSet rs = preparedStatement.executeQuery();
+
+            //Store the record in a message object and return it to the service. 
+            while(rs.next()){
+                Message message = new Message(rs.getInt("message_id"),
+                        rs.getInt("posted_by"),
+                        rs.getString("message_text"),
+                        rs.getLong("time_posted_epoch"));
+                return message;
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    //Delete One Message
+    public Message deleteMessageById(int message_id){
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            //Create a prepared Statement
+            String sql = "DELETE FROM message WHERE message_id = (?);";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, message_id);
+
+            //Execute Statement, it will return an int number of rows effected, which will be 1 or 0 depending upon whether that message exists in the database. 
+            int rowsDeleted = preparedStatement.executeUpdate();
+
+            //If we successfully deleted a row, run a select query to return the deleted message
+            if (rowsDeleted > 0) {
+                //Create a prepared Statement
+                String sql2 = "SELECT * FROM message WHERE message_id = ?";
+                PreparedStatement preparedStatement2 = connection.prepareStatement(sql2);
+                preparedStatement2.setInt(1, message_id);
+
+                //Execute Statement
+                ResultSet rs = preparedStatement2.executeQuery();
+
+                //Store the deleted record in a message object and return it to the service. 
+                while(rs.next()){
+                    Message deletedMessage = new Message(rs.getInt("message_id"),
+                            rs.getInt("posted_by"),
+                            rs.getString("message_text"),
+                            rs.getLong("time_posted_epoch"));
+                    return deletedMessage;
+                }
+            }
+
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        //Return null if no message was 
+        return null;
+    }
+
+
+    //Update Message
+    public void updateMessage(int message_id, String message_text){
+        Connection connection = ConnectionUtil.getConnection();
+        
+        try {
+            //Create a prepared Statement
+            String sql = "UPDATE message SET message_text = ? WHERE message_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            //write PreparedStatement setString and setInt methods here.
+            preparedStatement.setString(1, message_text);
+            preparedStatement.setInt(2, message_id);
+
+            //Execute Statement
+            preparedStatement.executeUpdate();
+
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
+
+
+}
+
+
+
+// Delete a Message Given Message Id
+// As a User, I should be able to submit a DELETE request on the endpoint DELETE localhost:8080/messages/{message_id}.
+// The deletion of an existing message should remove an existing message from the database. 
+// If the message existed, the response body should contain the now-deleted message. 
+// The response status should be 200, which is the default.
+// If the message did not exist, the response status should be 200, but the response body should be empty. 
+// This is because the DELETE verb is intended to be idempotent, ie, 
+// multiple calls to the DELETE endpoint should respond with the same type of response.
+
